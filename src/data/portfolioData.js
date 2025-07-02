b946c21eb876790d1cba333da764b86b344f957e
Gaspar Giménez <gaspar.gimenez8@gmail.com>
2025-07-01 21:43:15 -0300

    finish monolito

---
+++ src/data/portfolioData.js
@@ -0,0 +1,218 @@
+const portfolioData = {
+            en: {
+                about: {
+                    title: "About Me",
+                    description: "IT Technician, Computer Science student, and full-stack developer. A proactive individual interested in leading projects and a great team player. With over 4 years of experience in the field, my main skills lie in efficiently and scalably solving computer-related problems and web development, with the ability to adapt to different environments."
+                },
+                experience: {
+                    title: "Experience",
+                    items: [
+                        {
+                            company: "Bravo",
+                            jobTitle: "Co-founder & CTO",
+                            location: "Rosario, Argentina",
+                            fromDate: "Mar 2023",
+                            toDate: "Current",
+                            description: "As a Co-Founder of a tech startup, I handle a diverse range of tasks, including implementing new code, managing the database, deploying to production, and organizing the team. I led my team in developing our main web application using a Laravel - Next.js - MySQL stack, with Docker for development and GitHub Actions for production deployment, alongside automated tests. In addition to tech-related responsibilities, I contribute to idea validation, the marketing process, and the overall creative process."
+                        },
+                        {
+                            company: "Wymaq",
+                            jobTitle: "Full-stack Developer",
+                            location: "Buenos Aires, Argentina",
+                            fromDate: "Oct 2022",
+                            toDate: "Jan 2024",
+                            description: "At WyMaq, I contributed to the company’s flagship projects by delivering efficient and scalable solutions. I led the transition from the company’s previous tech stack to a more efficient and scalable framework. As a full-stack developer, I worked with technologies such as Next.js and AngularJS (https://www.wymaq.com.ar/), both integrated with a Laravel 9 RESTful API. Additional technologies included Docker, GitLab, and MySQL."
+                        },
+                        {
+                            company: "Avalian",
+                            jobTitle: "Professionalizing practices - Intern",
+                            location: "Rosario, Argentina",
+                            fromDate: "May 2022",
+                            toDate: "Sep 2022",
+                            description: "Worked as a database analyst with technologies like PL/SQL and Oracle Forms. First worked supporting the users, then refactoring legacy code, and finally generating new implementations for the core system of the company."
+                        },
+                        {
+                            company: "Scale AI",
+                            jobTitle: "Software Developer",
+                            location: "Remote",
+                            fromDate: "Jan 2024",
+                            toDate: "Nov 2024",
+                            description: "Worked supervising and generating code for the training of different LLMs related to data science. Using mostly Python with libraries like Pandas or Matplotlib."
+                        },
+                        {
+                            company: "GES Particulares",
+                            jobTitle: "Co-founder and Teacher",
+                            location: "Rosario, Argentina",
+                            fromDate: "Jun 2021",
+                            toDate: "Jan 2024",
+                            description: "Founded a tutoring academy focused on high school students. Taught subjects like Math, Computer Science, and Technical Drawing. Managed a team of over 10 teachers and helped more than 150 students."
+                        }
+                    ]
+                },
+                education: {
+                    title: "Education & Languages",
+                    items: [
+                        {
+                            place: "Faculty of Exact Sciences. National University of Rosario",
+                            description: "Master's Degree in Computer Science",
+                            location: "Rosario, Argentina",
+                            fromDate: "2023",
+                            toDate: "2028 (Expected)",
+                            data: ["GPA: 8.62/10", "Student of a 5-year degree in Computer Science.", "Percentage of progress: 40.63%"]
+                        },
+                        {
+                            place: "Asociación Cultural Inglesa",
+                            description: "B2 FCE FIRST Certificate",
+                            location: "Rosario, Argentina",
+                            fromDate: "2023",
+                            toDate: "",
+                            data: ["Score: 173", "Upper-Intermediate Level"]
+                        }
+                    ]
+                },
+                skills: {
+                    title: "Skills & Technologies",
+                    items: [
+                        "I possess expertise in web development, with the adaptability to thrive in diverse environments, showcasing a versatile approach to tackling challenges.",
+                        "I have experience working with the following programming languages: JavaScript, PHP, C, SQL (In Oracle or MySQL databases), Python, and Haskell. Aside, I have worked with technologies like: HTML, Bootstrap, TailwindCSS, GIT, Docker, Ajax.",
+                        "On the backend side, I have developed projects based on the following frameworks: Laravel (REST API), NodeJS with Express, and Django.",
+                        "Regarding front-end development, I have worked on NextJS and AngularJS projects.",
+                        "In addition, I have a smooth handling of OS like Windows or Linux (terminal).",
+                        "I would like to learn more about technologies like Machine Learning and Blockchain."
+                        
+                    ]
+                },
+                references: {
+                    title: "References",
+                    items: [
+                        {
+                            name: "Lic. Alejandro Sebastián González",
+                            title: "Systems Development Leader",
+                            email: "agonzales@avalian.com",
+                            company: "Avalian"
+                        },
+                        {
+                            name: "Téc. Federico Zurita",
+                            title: "IT Leader",
+                            email: "fzurita@avalian.com",
+                            company: "Avalian"
+                        },
+                        {
+                            name: "Eng. Alejandro Guillermo Schujman",
+                            title: "Professor",
+                            email: "alejandroschujman@ips.edu.ar",
+                            company: "IPS (Instituto Politécnico Superior)"
+                        }
+                    ]
+                }
+            },
+            es: {
+                about: {
+                    title: "Sobre Mí",
+                    description: "Técnico Informaático, desarrollador Full-Stack y estudiante de Licenciatura en Ciencias de la Computación. Una persona proactiva, interesado en dirigir proyectos e indagar en el mundo de la investigación. Con más de 4 años de experiencia en el campo, mis principales habilidades radican en resolver problemas informáticos de manera eficiente y escalable, así como en el desarrollo web, con la capacidad de adaptarme a diferentes entornos"
+                },
+                experience: {
+                    title: "Experiencia",
+                    items: [
+                        {
+                            company: "Bravo",
+                            jobTitle: "Co-fundador y CTO",
+                            location: "Rosario, Argentina",
+                            fromDate: "Mar 2023",
+                            toDate: "Actual",
+                            description: "Como cofundador de una startup tecnológica, manejo una amplia variedad de tareas, incluyendo la implementación de nuevo código, la gestión de la base de datos, el despliegue a producción y la organización del equipo. Lideré a mi equipo en el desarrollo de nuestra aplicación web principal utilizando un stack de Laravel - Next.js - MySQL, con Docker para el desarrollo y GitHub Actions para el despliegue en producción, junto con pruebas automatizadas. Además de las responsabilidades tecnológicas, contribuyo a la validación de ideas, al proceso de marketing y al proceso creativo en general."
+                        },
+                        {
+                            company: "Wymaq",
+                            jobTitle: "Desarrollador Full-Stack",
+                            location: "Buenos Aires, Argentina",
+                            fromDate: "Oct 2022",
+                            toDate: "Ene 2024",
+                            description: "En WyMaq, contribuí a los proyectos principalesde la empresa brindando soluciones eficientes y escalables. Lideré la transición del stack tecnológico anterior a un framework más eficiente y escalable. Como desarrollador full-stack, trabajé con tecnologías como Next.js y AngularJS (https://www.wymaq.com.ar/), ambos integrados con una API RESTful de Laravel 9. Las tecnologías adicionales incluyeron Docker, GitLab y MySQL."
+                        
+                        },
+                        {
+                            company: "Avalian",
+                            jobTitle: "Prácticas Profesionalizantes",
+                            location: "Rosario, Argentina",
+                            fromDate: "May 2022",
+                            toDate: "Sep 2022",
+                            description: "Trabajé como analista de bases de datos con tecnologías como PL/SQL y Oracle Forms. Primero trabajé apoyando a los usuarios, luego refactorizando código legado y finalmente generando nuevas implementaciones para el sistema central de la empresa."
+                        },
+                        {
+                            company: "Scale AI",
+                            jobTitle: "Desarrollador de Software",
+                            location: "Remoto",
+                            fromDate: "Ene 2024",
+                            toDate: "Nov 2024",
+                            description: "Trabajé supervisando y generando código para el entrenamiento de diferentes LLMs relacionados con la ciencia de datos. Usando principalmente Python con bibliotecas como Pandas o Matplotlib."
+                        },
+                        {
+                            company: "GES Particulares",
+                            jobTitle: "Co-fundador y Docente",
+                            location: "Rosario, Argentina",
+                            fromDate: "Jun 2021",
+                            toDate: "Ene 2024",
+                            description: "Fundé una academia de tutorías enfocada en estudiantes de secundaria. Enseñé materias como Matemáticas, Informática y Dibujo Técnico. Dirigí un equipo de más de 10 docentes y ayudé a más de 150 estudiantes."
+                        }
+                    ]
+                },
+                education: {
+                    title: "Educación e Idiomas",
+                    items: [
+                        {
+                            place: "Facultad de Ciencias Exactas. Universidad Nacional de Rosario",
+                            description: "Licenciatura en Ciencias de la Computación",
+                            location: "Rosario, Argentina",
+                            fromDate: "2023",
+                            toDate: "2028 (Esperado)",
+                            data: ["Promedio: 8.62", "Estudiante de tercer año.", "Porcentaje de avance: 40.63%"]
+                        },
+                        {
+                            place: "Asociación Cultural Inglesa",
+                            description: "Certificado B2 FCE FIRST",
+                            location: "Rosario, Argentina",
+                            fromDate: "2023",
+                            toDate: "",
+                            data: ["Puntuación: 173", "Certificado por Cambridge", "Nivel Intermedio Superior"]
+                        }
+                    ]
+                },
+                skills: {
+                    title: "Habilidades y Tecnologías",
+                    items: [
+                        "Poseo experiencia en desarrollo web, con la capacidad de adaptarme a diversos entornos, demostrando un enfoque versátil para afrontar desafíos.",
+                        "Tengo experiencia trabajando con los siguientes lenguajes de programación: JavaScript, PHP, C, SQL (en bases de datos Oracle o MySQL), Python y Haskell. Además, he trabajado con tecnologías como HTML, Bootstrap, TailwindCSS, GIT, Docker y Ajax.",
+                        "En el lado backend, he desarrollado proyectos basados en los siguientes frameworks: Laravel (REST API), Node.js con Express y Django. En el frontend, he trabajado en proyectos con Next.js y AngularJS.",
+                        "Tengo experiencia dando clases, atendiendo al público y organizando el trabajo de mis equipos.",
+                        "Además, tengo un manejo fluido de sistemas operativos como Windows o Linux (terminal).",
+                        "Me gustaría aprender más sobre tecnologías como Machine Learning y Blockchain."
+                    ]
+                },
+                references: {
+                    title: "Referencias",
+                    items: [
+                        {
+                            name: "Lic. Alejandro Sebastián González",
+                            title: "Líder de Desarrollo de Sistemas",
+                            email: "agonzales@avalian.com",
+                            company: "Avalian"
+                        },
+                        {
+                            name: "Téc. Federico Zurita",
+                            title: "Líder de IT",
+                            email: "fzurita@avalian.com",
+                            company: "Avalian"
+                        },
+                        {
+                            name: "Ing. Alejandro Guillermo Schujman",
+                            title: "Profesor",
+                            email: "alejandroschujman@ips.edu.ar",
+                            company: "Universidad Nacional de Rosario"
+                        }
+                    ]
+                }
+            }
+        };
+
+export default portfolioData;
